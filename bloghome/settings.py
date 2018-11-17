57338f962cddfbc0166be049ad081402fbd8536c
Raúl Lezama Hernández <30272518+raulezama@users.noreply.github.com>
2018-11-16 20:19:12 -0600

    Update settings.py

--- bloghome/settings.py
+++ bloghome/settings.py
@@ -25,14 +25,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k$jla@y^x&qucu=$!8o^x_vmb%=j3wqc$_^2*r4+p04o6ox0v&'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
